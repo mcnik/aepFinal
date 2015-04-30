cc5f1791daf043712891658016604ae9a3bd29c5
nikhil <nikhilcm42@gmail.com>
2015-04-30 14:27:45 -0700

    test 7 added, all tests updated for readability

--- test/app/nikhil/date/DateValidatorTest.java
+++ test/app/nikhil/date/DateValidatorTest.java
@@ -8,44 +8,51 @@ import static org.junit.Assert.*;
 public class DateValidatorTest {
 
     @Test
-    public void testDateIsNull() {
+    public void nullIsInvalid() {
         DateValidator dateValidator = new DateValidator();
         assertFalse(dateValidator.validator(null, "dd/MM/yyyy"));
     }
 
     @Test
-    public void testDayIsInvalid() {
+    public void thirtyTwoIsInvalidAsDay() {
         DateValidator dateValidator = new DateValidator();
         assertFalse(dateValidator.validator("32/02/2012", "dd/MM/yyyy"));
     }
 
     @Test
-    public void testMonthIsInvalid() {
+    public void twentyIsInvalidAsMonth() {
         DateValidator dateValidator = new DateValidator();
         assertFalse(dateValidator.validator("11/20/2014", "dd/MM/yyyy"));
     }
 
     @Test
-    public void testYearIsInvalid() {
+    public void threeDigitNumberInvalidAsYear() {
         DateValidator dateValidator = new DateValidator();
         assertFalse(dateValidator.validator("31/20/191", "dd/MM/yyyy"));
     }
 
     @Test
-    public void testDateFormatIsInvalid() {
+    public void yyyyMMDDIsInvalid() {
         DateValidator dateValidator = new DateValidator();
         assertFalse(dateValidator.validator("2015/02/02", "dd/MM/yyyy"));
     }
 
     @Test
-    public void testForLeapYear() {
+    public void twentyTwelveHasFebTwentyNine() {
         DateValidator dateValidator = new DateValidator();
         assertTrue(dateValidator.validator("29/02/2012", "dd/MM/yyyy"));
     }
 
     @Test
-    public void testDateFeb29_2011() {
+    public void twentyElevenDoesNotHaveFebTwentyNine() {
         DateValidator dateValidator = new DateValidator();
         assertFalse(dateValidator.validator("29/02/2011", "dd/MM/yyyy"));
     }
+
+    @Test
+    public void twentyElevenHasFebTwentyEight() {
+        DateValidator dateValidator = new DateValidator();
+        assertTrue(dateValidator.validator("28/02/2011", "dd/MM/yyyy"));
+    }
+
 }
