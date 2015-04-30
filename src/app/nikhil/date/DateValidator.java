2461d7702e5115b7f83915a92702ebd25fdfbf38
nikhil <nikhilcm42@gmail.com>
2015-04-30 02:22:32 -0700

    init repo

---
+++ src/app/nikhil/date/DateValidator.java
@@ -0,0 +1,27 @@
+package app.nikhil.date;
+
+import java.text.ParseException;
+import java.text.SimpleDateFormat;
+import java.util.Date;
+
+public class DateValidator {
+
+    public boolean validator(String date, String format) {
+
+        if (date == null) {
+            return false;
+        }
+
+        SimpleDateFormat sdf = new SimpleDateFormat(format);
+        sdf.setLenient(false);
+
+        try {
+            Date d = sdf.parse(date);
+            System.out.println(d);
+        } catch (ParseException e) {
+            e.printStackTrace();
+            return false;
+        }
+        return true;
+    }
+}
